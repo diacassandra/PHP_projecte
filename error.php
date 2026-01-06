b628bb18948400041229751f6d61ac4292ea849a
carlasabata <2024_carla.sabata@iticbcn.cat>
2026-01-06 01:42:27 +0100

    est

---
+++ error.php
@@ -0,0 +1,6 @@
+# Missatges d'error específics per a cada situació
+# Validacions en tots els formularis:
+Camps buits
+Formats incorrectes (email, dates, números)
+Duplicats (usuari ja existeix, ISBN repetit, etc.)
+Relacions (no es pot eliminar un element si té elements relacionats)
---
+++ header.php
@@ -0,0 +1,6 @@
+# inclos  atotes les pàgs
+# si l'user no esta logat, nom app
+# si l'user esta logat, nom/email d'user i el seu rol
+
+# control d'accés: Comprovar en cada pàgina interna si existeix la variable de sessió de l'usuari
+# Si no existeix, redirigir a login.php
